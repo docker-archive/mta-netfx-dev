ce636dac4933b34002745f341ce29d31eec50ad7
Elton Stoneman <elton@sixeyed.com>
2018-01-24 19:29:21 +0000

    Add homepage component

--- src/SignUp/SignUp.Web/Default.aspx.cs
+++ src/SignUp/SignUp.Web/Default.aspx.cs
@@ -9,5 +9,23 @@ namespace SignUp.Web
 {
     public partial class _Default : Page
     {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var homepageUrl = ConfigurationManager.AppSettings["Homepage.Url"];
+            if (!string.IsNullOrEmpty(homepageUrl))
+            {
+                Log.Info($"Loading homepage content from: {homepageUrl}");
+                Response.Clear();
+                var request = HttpWebRequest.Create(homepageUrl);
+                var response = request.GetResponse();
+                using (var stream = response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    var html = reader.ReadToEnd();
+                    Response.Write(html);
+                }
+                Response.End();
+            }
+        }
     }
 }
